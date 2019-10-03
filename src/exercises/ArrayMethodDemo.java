079131ca4ac27b9449e5fd633088d3a810bc3e43
Chris Molis <chrismolis12@gmail.com>
2019-10-03 09:55:08 -0500

    pain commit ;(

---
+++ src/exercises/ArrayMethodDemo.java
@@ -0,0 +1,16 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class ArrayMethodDemo {
+    public static void main(String[] args)
+    {
+        int arrayNumbers;
+        int[] tenArray = new int[10];
+        for(int i = 0; i > 10; ++i) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter 10 Values here");
+            arrayNumbers = input.nextInt();
+        }
+    }
+}
--- src/exercises/DistanceFromAverage.java
+++ src/exercises/DistanceFromAverage.java
@@ -5,18 +5,36 @@ import java.util.Scanner;
 public class DistanceFromAverage {
     public static void main(String[] args)
     {
-        double userEntry;
-
+        double values;
+        double loops;
+        double total = 0;
+        double counter = 1;
+        double avg;
+        double difference;
         double[] numArray = new double[20];
 
-        for(int i = 0; i < numArray.length; ++i)
-        {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter your value" + (i+1) + "here >>>" );
-            userEntry = input.nextDouble();
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many values are you entering? (Up to 20)");
+        loops = input.nextDouble();
+        if(loops <= 20) {
+                for (int i = 0; i < loops; ++i) {
+                    System.out.println("Enter your value " + (i + 1) + " here (up to 20) >>>");
+                    values = input.nextDouble();
+                    numArray[i] = values;
+                }
+                for (int j = 0; j < loops; ++j) {
+                    total = total + numArray[j];
+                    avg = total / counter;
+                    difference = numArray[j] - avg;
+                    ++counter;
+                    System.out.println("The entered number is " + numArray[j] + ", it is "
+                            + difference + " away from the average of " + avg);
+                }
         }
-
+        else
+        {
+            System.out.println("INVALID VALUE!");
+        }
 
     }
 
