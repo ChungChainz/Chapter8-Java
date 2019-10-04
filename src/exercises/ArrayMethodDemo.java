0a644fe9723bb0d6683b2b02f7f498112f62f645
Chris Molis <chrismolis12@gmail.com>
2019-10-04 09:55:06 -0500

    progress push:computer:

--- src/exercises/ArrayMethodDemo.java
+++ src/exercises/ArrayMethodDemo.java
@@ -7,10 +7,72 @@ public class ArrayMethodDemo {
     {
         int arrayNumbers;
         int[] tenArray = new int[10];
-        for(int i = 0; i > 10; ++i) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter 10 Values here");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type in 10 integer numbers: ");
+        for(int i = 0; i < 10; ++i) {
+            System.out.println("Enter Integers here >>> ");
             arrayNumbers = input.nextInt();
+            tenArray[i] = arrayNumbers;
+        }
+        showIntegers(tenArray);
+        reverseIntegers(tenArray);
+        sumIntegers(tenArray);
+        limitedIntegers(tenArray);
+        higherIntegers(tenArray);
+    }
+    public static void showIntegers(int[] tenArray)
+    {
+        System.out.println("In Order:\n");
+        for(int v = 0; v < 10; ++v ) {
+            System.out.println(tenArray[v]);
+        }
+    }
+    public static void reverseIntegers(int[] tenArray)
+    {
+        System.out.println("Reverse Order:\n");
+        for(int j = 9; j > 0; --j )
+        {
+            System.out.println(tenArray[j]);
+        }
+    }
+    public static void sumIntegers(int[] tenArray)
+    {
+       int sum = 0;
+        for (int l = 0; l < 10; ++l)
+        {
+            sum = tenArray[l] + sum;
+        }
+        System.out.println("Sum of the Integers =: " + sum);
+    }
+    public static void limitedIntegers(int[] tenArray)
+    {
+        int limit = 20;
+
+        System.out.println("Numbers under the limit '20':\n");
+
+        for (int n = 0; n < 10; ++n)
+        {
+            if(limit > tenArray[n])
+          {
+              System.out.println(tenArray[n]);
+          }
+        }
+
+    }
+    public static void higherIntegers(int[] tenArray) {
+        int average;
+        int total = 0;
+
+        System.out.println("Numbers above the average:\n");
+
+        for (int a = 0; a < 10; ++a) {
+            total = tenArray[a] + total;
+            }
+        average = total / tenArray.length;
+        for(int b = 0; b < 10; ++b)
+            if (average < tenArray[b]) {
+            System.out.println(tenArray[b]);
         }
     }
 }
---
+++ src/game_zone/Quiz.java
@@ -0,0 +1,34 @@
+package game_zone;
+
+public class Quiz {
+    public static void main(String[] args)
+    {
+        int wrong = 0;
+        int correct = 0;
+        String[] questionArray = new String[]{
+                "1. What NFL team won the Super Bowl in 2017?" +
+                        "\nA. Patriots\nB. Falcons\nC. Colts",
+                "2. Who is the NFL all time leader in passing TD's?" +
+                        "\nA. Brett Farve\nB. Peyton Manning\nC. Dan Marino",
+                "3. Who had the most amount of fantasy points in PPR leagues in 2018?" +
+                        "\nA. Patrick Mahomes\nB. Todd Gurley C. Christian McCaffery",
+                "4. Where did Aaron Rodgers go to college?" +
+                        "\nA. UCLA\nB. Nevada\nC. California-Berkeley",
+                "5. Where is the Oakland Raiders planning on relocating during the 2020 season?" +
+                        "\nA. London\nB. Toronto\nC. Las Vegas",
+                "6. Which college had the most players drafted in the 2019 Draft?" +
+                        "\nA. Ohio State\nB. Oklahoma\nC. Alabama",
+                "7. What players was known for 'salsa' dancing as their TD celebration?" +
+                        "\nA. Santana Moss\nB. Victor Cruz\nC. Mark Sanchez",
+                "8. What famous phrase did Matt Hasselbeck say in OT during the '04 Wild card game vs the GB Packers? " +
+                        "\nA. We'll take the ball, and we're gonna score\nB. Victor Cruz\nC. Mark Sanchez",
+                "9. What teams were playing in the game known as the 'Tuck Rule' Game",
+                "10. Which of the following teams have never won a Super Bowl?"};
+        String[] correctAnswer = new String[]{"A","B","A","C","C","C","B","A","C","B"};
+        for(int i = 0; i > 10; ++i)
+        {
+
+        }
+
+    }
+}
